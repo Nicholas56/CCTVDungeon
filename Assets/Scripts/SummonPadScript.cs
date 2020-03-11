60b12c5982b24297bb8a8e0e0d340a9a75daf255
Nicholas56 <nick.easterby@gmail.com>
2020-03-11 15:57:18 +0000

    Added the summon pad script
    Tested the vr components
    adjusted controller options

---
+++ Assets/Scripts/SummonPadScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SummonPadScript : MonoBehaviour
+{
+    public Transform controlPad;
+    public Transform player;
+    bool timeToGrab = false;
+    float timer;
+    float delay = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the grab boolean is on for the first time, the delay is set and added to the timer
+        if (timeToGrab && delay==0f)
+        {
+            delay = 1f;
+            timer = Time.time + delay;            
+        }
+        //Once the timer runs out the bool and delay are reset
+        if (Time.time > timer)
+        {
+            timeToGrab = false;
+            delay = 0f;
+        }
+    }
+
+    public void BeginToSummon()
+    {
+        //This is run once to set the bool, then run a second time to move the object into place
+        if (timeToGrab)
+        {
+            if (player == null) { player = GameObject.FindGameObjectWithTag("Spawn").transform; }
+            Vector3 pos = player.position;
+            controlPad.position = pos;
+        }
+        timeToGrab = true;
+        Debug.Log("Function called");
+    }
+}
